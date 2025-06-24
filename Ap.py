8be899c9f3b9f14ffe18f6aa3b701481ae8c651e
Jivs2000 <vishu.magon@gmail.com>
2025-06-24 22:58:34 +0530

    Update Ap.py

--- Ap.py
+++ Ap.py
@@ -10,9 +10,6 @@ ed = st.selectbox("Education Level (1=Low, 5=High)", list(range(1, 6)))
 employ = st.slider("Years of Employment", 0, 40, 5)
 address = st.slider("Years at Address", 0, 40, 5)
 income = st.number_input("Annual Income (in thousands)", min_value=0, value=50)
-dti = st.number_input("Debt to Income Ratio", min_value=0.0, value=10.0)
-cdr = st.number_input("Credit to Debt Ratio", min_value=0.0, value=1.5)
-othdebt = st.number_input("Other Debt", min_value=0.0, value=3.0)
 
 # --- Simple rule-based scoring ---
 score = 0
@@ -36,21 +33,8 @@ if income >= 50:
 elif income >= 30:
     score += 5
 
-# Debt-to-Income Ratio (lower is better)
-if dti < 15:
-    score += 10
-elif dti < 30:
-    score += 5
 
-# Credit-to-Debt Ratio (higher is better)
-if cdr >= 1:
-    score += 10
-elif cdr >= 0.5:
-    score += 5
 
-# Other Debt (lower is better)
-if othdebt < 5:
-    score += 5
 
 # --- Classification ---
 if score >= 40:
